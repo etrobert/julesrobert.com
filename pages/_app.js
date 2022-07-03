3205eb5746ebf3a4f06f76a9e4863e2a4722986a
erobert <etiennerobert33@gmail.com>
2022-07-03 17:10:33 +0200

    Fix Head position

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import "../style.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Head>
-      <title>Étienne Robert</title>
+    <>
+      <Head>
+        <title>Étienne Robert</title>
+      </Head>
       <Component {...pageProps} />
-    </Head>
+    </>
   );
 }
