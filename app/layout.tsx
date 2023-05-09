1ecfdfec177a4ad7ce0415186580b14e5b5af1c7
erobert <etiennerobert33@gmail.com>
2023-05-09 22:24:33 +0200

    Add layout.ts in app

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Étienne Robert",
+  description: "Étienne Robert's personal website",
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
