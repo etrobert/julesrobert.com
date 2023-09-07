4328f23e6414c2c3460efcb3d34238d0590cc5dd
Étienne Robert <etiennerobert33@gmail.com>
2023-09-07 11:28:54 +0200

    Dynamic icons (#7)
    * Migrate the github icon to react
    
    * Migrate Linkedin icon
    
    * Migrate instagram icon

--- components/BusinessCard.tsx
+++ components/BusinessCard.tsx
@@ -1,25 +1,25 @@
 import Link from 'next/link';
 import styles from './BusinessCard.module.scss';
+import GithubIcon from './icons/GithubIcon';
+import LinkedinIcon from './icons/LinkedinIcon';
+import InstagramIcon from './icons/InstagramIcon';
 
 const BusinessCard = () => (
   <div className={styles.container}>
     <h1 className={styles.heading}>Étienne Robert</h1>
     <div className={styles.links}>
+      <Link target="_blank" href="https://github.com/etrobert">
+        <GithubIcon />
+      </Link>
       <Link
-        className={styles['github-link']}
-        target="_blank"
-        href="https://github.com/etrobert"
-      />
-      <Link
-        className={styles['linkedin-link']}
         target="_blank"
         href="https://www.linkedin.com/in/etienne-robert-dev/"
-      />
-      <Link
-        className={styles['instagram-link']}
-        target="_blank"
-        href="https://www.instagram.com/etrobert_"
-      />
+      >
+        <LinkedinIcon />
+      </Link>
+      <Link target="_blank" href="https://www.instagram.com/etrobert_">
+        <InstagramIcon />
+      </Link>
     </div>
   </div>
 );
