b5b8f477fb0cfeaf53b153d57290f7c13021072e
Etienne Robert <etiennerobert33@gmail.com>
2024-05-06 16:17:28 +0200

    Add labels to links

--- components/IconLink.tsx
+++ components/IconLink.tsx
@@ -5,13 +5,15 @@ import type { ReactNode } from 'react';
 type Props = {
   href: string;
   icon: ReactNode;
+  label: string;
 };
 
-const IconLink = ({ href, icon }: Props) => (
+const IconLink = ({ href, icon, label }: Props) => (
   <Link
     target="_blank"
     href={href}
     className="w-[2em] opacity-80 hover:opacity-100 transition-opacity duration-300"
+    aria-label={label}
   >
     {icon}
   </Link>
--- components/Links.tsx
+++ components/Links.tsx
@@ -5,12 +5,18 @@ import IconLink from './IconLink';
 
 const Links = () => (
   <div className={'flex justify-center gap-[5vw]'}>
-    <IconLink href="https://github.com/etrobert" icon={<GithubIcon />} />
     <IconLink
+      label="Github Profile"
+      href="https://github.com/etrobert"
+      icon={<GithubIcon />}
+    />
+    <IconLink
+      label="Linkedin Profile"
       href="https://www.linkedin.com/in/etienne-robert-dev/"
       icon={<LinkedinIcon />}
     />
     <IconLink
+      label="Instagram Profile"
       href="https://www.instagram.com/etrobert_"
       icon={<InstagramIcon />}
     />
