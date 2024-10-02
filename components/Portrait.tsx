20d85ea4ee26acb998ee603e26fe25e2879eb23d
Etienne Robert <etiennerobert33@gmail.com>
2024-10-02 17:34:48 +0200

    Update portrait size

--- components/Portrait.tsx
+++ components/Portrait.tsx
@@ -8,8 +8,8 @@ const Portrait = () => (
       styles.portrait,
       'border-2 border-stone-900 dark:border-stone-50',
     ].join(' ')}
-    width={1365 / 5}
-    height={2048 / 5}
+    width={799 / 2.5}
+    height={800 / 2.5}
     alt="Portrait of the author"
   />
 );
