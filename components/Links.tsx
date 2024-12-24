19a39cd2ee85885ca9dbb43e101f20fc4b867929
Etienne Robert <etiennerobert33@gmail.com>
2024-12-24 19:07:47 +0100

    Add instagram link

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,5 +1,6 @@
 import SpotifyIcon from './icons/SpotifyIcon';
 import IconLink from './IconLink';
+import InstagramIcon from './icons/InstagramIcon';
 
 const Links = () => (
   <div className={'flex justify-center gap-[5vw]'}>
@@ -7,6 +8,11 @@ const Links = () => (
       label="Spotify Profile"
       href="https://open.spotify.com/user/1165742894?si=c3dfeed5c421480f"
       icon={<SpotifyIcon />}
+    />
+    <IconLink
+      label="Instagram Profile"
+      href="https://www.instagram.com/jules_rbt2"
+      icon={<InstagramIcon />}
     />
   </div>
 );
