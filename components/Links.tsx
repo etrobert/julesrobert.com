3597e49a1fe80fac67fa32ca669bdeddee685d31
Etienne Robert <etiennerobert33@gmail.com>
2024-12-25 00:33:21 +0100

    Update spotify url

--- components/Links.tsx
+++ components/Links.tsx
@@ -6,7 +6,7 @@ const Links = () => (
   <div className={'flex justify-center gap-[5vw]'}>
     <IconLink
       label="Spotify Profile"
-      href="https://open.spotify.com/user/1165742894?si=c3dfeed5c421480f"
+      href="https://open.spotify.com/user/31txgikw4bbkljisga7ebh5tg4vu?nd=1&dlsi=adeba3d7536d49f1"
       icon={<SpotifyIcon />}
     />
     <IconLink
